e293abb54a2a50cf2c6efec6829791cb9a42a64b
chanho <dirtyface1234@naver.com>
2024-07-26 23:00:26 +0900

    float and complex 실습

---
+++ goGrammar/float_complex/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var f1 float32 = 3.14
+	var f2 float64 = 3.141592654589793
+
+	fmt.Println(f1)
+	fmt.Println(f2)
+
+	var c1 complex64 = complex(1.5, 2.5)
+	var c2 complex128 = complex(1.5, 2.5)
+
+	fmt.Println(c1)
+	fmt.Println(c2)
+
+	fmt.Println(real(c1))
+	fmt.Println(imag(c1))
+
+	fmt.Println(real(c2))
+	fmt.Println(imag(c2))
+
+	var c3 complex128 = complex(2.5, 3.5)
+	var c4 complex128 = complex(1.5, -1.5)
+
+	fmt.Println(c3 + c4)
+	fmt.Println(c3 - c4)
+	fmt.Println(c3 * c4)
+	fmt.Println(c3 / c4)
+
+}
